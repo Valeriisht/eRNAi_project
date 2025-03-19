d677021453b2b4bf0539eee16d7e9e2690bd2c4a
Valeriisht <138997713+Valeriisht@users.noreply.github.com>
2025-03-19 19:56:35 +0300

    Update installations.sh

--- installations.sh
+++ installations.sh
@@ -4,5 +4,6 @@ conda install bioconda::humann=3.9
 conda install bioconda::snakemake=8.30.0
 conda install conda-forge::datasets=16.40.1
 conda install bioconda::fastp=0.24.0 
-conda install bioconda::kraken2=2.14   
+conda install bioconda::kraken2=2.14  
+conda install bioconda::bracken=3.1
 
