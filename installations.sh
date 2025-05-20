6de2609a5a46466a82cf2cbb26ef9ee7bda710fb
Valeria <138997713+Valeriisht@users.noreply.github.com>
2025-05-20 14:47:54 +0300

    Update installations.sh

--- installations.sh
+++ installations.sh
@@ -1,2 +1,14 @@
-conda install python=3.10.14
-conda install bioconda::sra-tools
+conda install python=3.11
+conda install bioconda::sra-tools=3.2.0
+conda install bioconda::humann=3.9
+conda install bioconda::snakemake=8.30.0
+conda install conda-forge::datasets=16.40.1
+conda install bioconda::fastp=0.24.0 
+conda install bioconda::kraken2=2.14  
+conda install bioconda::bracken=3.1
+conda install bioconda::metaphlan
+conda install -c conda-forge coincbc
+conda install bioconda::ncbi-genome-download
+conda install bioconda::dsk=2.3.3
+conda install bioconda::blast=2.16.0
+conda install conda-forge::datasets=3.6.0
