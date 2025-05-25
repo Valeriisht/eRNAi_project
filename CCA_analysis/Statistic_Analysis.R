edc74bdf83038c6845d9026d97ef1aafaec9160c
Valeria <138997713+Valeriisht@users.noreply.github.com>
2025-05-25 23:09:14 +0300

    Update Statistic_Analysis.R

--- CCA_analysis/Statistic_Analysis.R
+++ CCA_analysis/Statistic_Analysis.R
@@ -57,16 +57,16 @@ for (i in 1:nrow(env_loadings)) {
     geom_point(data=as.data.frame(species_loadings), aes(x = CCA1, y = CCA2), col="gray",
                alpha = 0.6) +
     geom_point(data=as.data.frame(species_loadings[selected, ]), aes(x = CCA1, y = CCA2), 
-               col="purple", alpha = 0.8, size=5) +
+               col="#A47DAB", alpha = 0.8, size=5) +
     geom_point(data=as.data.frame(species_loadings[not_selected, ]), aes(x = CCA1, y = CCA2), 
-               col="orange", alpha = 0.8, size=5) + 
+               col="#FFE5B4", alpha = 0.8, size=5) + 
     geom_segment(aes(x = 0, y = 0, xend = v_norm[1,1], yend = v_norm[1,2]),
-                 arrow = arrow(length = unit(0.2, "cm")), color = "green") +
+                 arrow = arrow(length = unit(0.2, "cm")), color = "aquamarine3") +
     geom_text(aes(x=target_microbe[1, 1], y=target_microbe[1, 2]), label=org_name,
-              color = "red", hjust = 1.1, vjust = 1.1) +
-    geom_abline(intercept = 0, slope = slope1, linetype = "dashed", color = "green")+
-    geom_abline(intercept = 0, slope = slope2, linetype = "dashed", color = "green")+
-    labs(title = paste("Collinear transcripts from", org_name),
+              color = "red", hjust = 1.1, vjust = 1.1, fontface = "italic") +
+    geom_abline(intercept = 0, slope = slope1, linetype = "dashed", color = "aquamarine3")+
+    geom_abline(intercept = 0, slope = slope2, linetype = "dashed", color = "aquamarine3")+
+    labs(title = substitute(paste("Collinear transcripts from ", italic(x)), list(x = org_name)),
          x = "CCA1", y = "CCA2") +
     theme_minimal()
   
@@ -84,6 +84,7 @@ for (i in 1:nrow(env_loadings)) {
 bslast_res <- list()
 
 file_path <- "/nfs/home/emarenina/eRNAi_project/database_gallus/blast_res_1e-3/"
+
 file_pattern <- "*.txt"  
 
 
@@ -115,7 +116,7 @@ process__file <- function(file_path) {
 blast_data <- bind_rows(lapply(blast_list, process__file))
 
 
-##### find the intersection
+##### find the intersection ##### 
 
 blast_id <- blast_data[, c("sseqid", "Sample")]
 blast_id <- blast_id[blast_id$sseqid %in% rownames(species_loadings), ]
@@ -132,9 +133,11 @@ for (i in 1:length(org)) {
 blast_names <- names(blast_list)
 
 
+
 collinear_names <-names(collinear_results)
 against_collinear_names <- names(against_collinear_results)
 non_colinear_names <- names(non_colinear_results)
+
 
 common_list_names <- intersect(blast_names, collinear_names)
 common_list_names <- intersect(common_list_names, against_collinear_names)
@@ -232,7 +235,7 @@ calculate_overlaps <- function(org_data, func = "binomial") {
         c("similar", "not_similar")
       )
     )
-    
+    print(contingency_table)
     fisher_test <- fisher.test(contingency_table)
     
     return(list(
@@ -257,7 +260,7 @@ overlap_stats <- lapply(org_data_list, calculate_overlaps)
   
 
 
-###### vis ###### for each organism  ###### 
+###### vis ###### for each organism - binomial  ###### 
 library(ggplot2)
 library(gridExtra)
 
@@ -331,30 +334,33 @@ visualize_overlaps <- function(results, title = "Overlap Statistics") {
     
     overlap_data$p_label <- ifelse(overlap_data$p_value < 0.001, "***",
                                    ifelse(overlap_data$p_value < 0.01, "**",
-                                          ifelse(overlap_data$p_value < 0.05, "*", "ns")))
+                                          ifelse(overlap_data$p_value < 0.05, "*", "")))
     
     all_data <- rbind(all_data, overlap_data) 
     
   }
   
   
-  ggplot(all_data, aes(x = Organism, y = Count, fill = Group)) +
-    geom_bar(stat = "identity", position = position_dodge()) +
-    geom_text(aes(label = paste0(Count, "\n", p_label)), 
+  ggplot(all_data, aes(x = Organism %>% fct_reorder(Count), y = Count, fill = Group)) +
+    geom_col(position = position_dodge(width = 0.8), width = 0.75) +
+    geom_text(aes(label = paste0(p_label, "\n", Count)), 
               position = position_dodge(width = 0.9),
-              vjust = -0.5, size = 4) +
-    labs(title = "Overlapping Transcripts Across Microorganism, Binomial Test",
+              vjust = -0.1, hjust = 0.5, size = 5, lineheight = 0.9, color = "gray30") +
+    labs(title = "Overlapping Transcripts Across Microorganism by CCA, Binomial Test",
          x = "Microorganism",
          y = "Number of Overlapping Microorganism",
          fill = "Group Type") +
     theme_minimal() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1),
-          legend.position = "bottom") +
-    scale_fill_brewer(palette = "Accent") +
-    scale_y_continuous(expand = expansion(mult = c(0, 0.1)))
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "italic", size = 8),
+          legend.position = "bottom",
+          axis.title.y = element_text(margin = margin(r = 10)),
+          ) +
+    scale_fill_manual(values = c("#A47DAB", "#FFE5B4", "aquamarine3")) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
+    coord_cartesian(clip = "off")
 }
   
-gg <- visualize_overlaps(overlap_stats)
+(gg <- visualize_overlaps(overlap_stats))
 
 ggsave("binomial_cca.png", plot = gg, device = "png", 
        width = 10, height = 8)
@@ -381,36 +387,41 @@ visualize_fisher <- function(results) {
                      org_data$probabilities["p_against_collinear"],
                      org_data$probabilities["p_non_collinear"]),
       
-      p_value = rep(org_data$fisher_test$p.value, 3) )
-    
+      
+      p_value = rep(org_data$fisher_test$p.value %>% 
+                      p.adjust(method = "fdr"), 3) )
+     
     
     overlap_data$p_label <- ifelse(overlap_data$p_value < 0.001, "***",
                                    ifelse(overlap_data$p_value < 0.01, "**",
-                                          ifelse(overlap_data$p_value < 0.05, "*", "ns")))
+                                          ifelse(overlap_data$p_value < 0.05, "*", "")))
     
     all_data <- rbind(all_data, overlap_data) 
     
   }
 
   
-  ggplot(all_data, aes(x = Organism, y = Count, fill = Group)) +
-    geom_bar(stat = "identity", position = position_dodge()) +
-    geom_text(aes(label = paste0(Count, "\n", p_label)), 
+  ggplot(all_data, aes(x = Organism %>% fct_reorder(Count), y = Count, fill = Group)) +
+    geom_col(position = position_dodge(width = 0.8), width = 0.75)  +
+    geom_text(aes(label = paste0(p_label, "\n", Count)), 
               position = position_dodge(width = 0.9),
-              vjust = -0.5, size = 4) +
-    labs(title = "Overlapping Transcripts Across Microorganism, Fisher Test",
+              vjust = -0.1, hjust = 0.5, size = 5, lineheight = 0.9, color = "gray30") +
+    labs(title = "Overlapping Transcripts Across Microorganism by CCA, Fisher Test",
          x = "Microorganism",
          y = "Number of Overlapping Microorganism",
          fill = "Group Type") +
     theme_minimal() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1),
-          legend.position = "bottom") +
-    scale_fill_brewer(palette = "Set3") +
-    scale_y_continuous(expand = expansion(mult = c(0, 0.1)))
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "italic", size = 8),
+          legend.position = "bottom",
+          axis.title.y = element_text(margin = margin(r = 10)),
+    )+
+    scale_fill_manual(values = c("#A47DAB", "#FFE5B4", "aquamarine3")) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
+    coord_cartesian(clip = "off")
   
 } 
 
-fisher_cca <- visualize_fisher(overlap_stats_fisher)
+(fisher_cca <- visualize_fisher(overlap_stats_fisher))
 
 ggsave("fisher_cca.png", plot = fisher_cca, device = "png", 
        width = 10, height = 8)
@@ -439,7 +450,6 @@ for (i in 1:length(org_trans)) {
 }
 
 
-
 overlap_cca_transcriptome <-  function(table_blast, transcripts_cca, transcripts_all) {
   
   results <- list()
@@ -449,20 +459,24 @@ overlap_cca_transcriptome <-  function(table_blast, transcripts_cca, transcripts
       
     transcriptome_cca_target <- intersect(blast_hits, transcripts_cca)
     transcriptome_cca_other <- setdiff(transcripts_cca, blast_hits)
-    transcripts_non_cca_target <- intersect(blast_hits, setdiff(transcripts_all, transcripts_cca))
+    transcripts_non_cca_target <- setdiff(blast_hits, transcripts_cca)
     transcripts_non_cca_other <- setdiff(setdiff(transcripts_all, transcripts_cca), blast_hits)
     
     fisher_table <-matrix(
       c(
-        length(transcriptome_cca_target), length(transcriptome_cca_other), 
-        length(transcripts_non_cca_target), length(transcripts_non_cca_other)
+        length(transcriptome_cca_target), 
+        length(transcriptome_cca_other), 
+        length(transcripts_non_cca_target), 
+        length(transcripts_non_cca_other)
         ),
         nrow = 2,
-        dimnames = list(
-          c("cca_targets", "cca_other"),
-          c("not_cca_targets", "not_cca_other")
-        )
+      
+      dimnames = list(
+        "Transcript_type" = c("CCA", "non_CCA"), 
+        "BLAST_result" = c("targets", "no_targets")     
       )
+      )
+    print(fisher_table)
     fisher_dif = (fisher_table[1,1]/fisher_table[1,2])/
       (fisher_table[2,1]/fisher_table[2,2])
     
@@ -510,40 +524,26 @@ df <- df %>%
     TRUE ~ " ",
   ))
 
-g <- ggplot(df, aes(x=organism %>% fct_reorder(-log_pvalue), y=log_pvalue, fill = significance)) +
-  geom_bar(stat="identity", width = 0.8) +
-  scale_fill_manual(values=c(" " = "grey",  "." = "purple", "**" = "skyblue"),
-                    name = "Significance") +
+g <- ggplot(df, aes(x=organism %>% fct_reorder(-log_pvalue), y=log_pvalue, fill = log_pvalue)) +
+  geom_bar(stat="identity", width = 0.8)  +
   geom_hline(yintercept = -log10(0.05), color = "red", linetype = "dashed") +
   labs(title = "Fisher test adjusted p-values *(-log10 scale, fdr)*",
        y = "-log10 p-value",
        x = "Organism") +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle=45, hjust=1, face = "italic"),
+  scale_fill_gradient("log p-value", low = "#FFE5B4", high = "#A47DAB") + 
+  theme(axis.text.x = element_text(angle=80, hjust=1, face = "italic"),
         plot.title = ggtext::element_markdown()) +
   geom_text(aes(label = round(log_pvalue, 1)), 
             vjust = -0.5, size = 3.5, color = "black") 
   
 print(g)
 
-g_add <- ggplot(df, aes(x=organism %>% fct_reorder(-log_pvalue), y=fisher_dif, fill = log_pvalue)) +
-  geom_bar(stat="identity", width = 0.8) +
-  labs(title = "Fisher test adjusted p-values *(-log10 scale, fdr)*",
-       y = "targets in/out CCA",
-       x = "Organism") +
-  theme_minimal() +
-  scale_fill_gradient("log p-value", low = "white", high = "darkgreen") +
-  theme(axis.text.x = element_text(angle=45, hjust=1, face = "italic"),
-        plot.title = ggtext::element_markdown()) +
-  geom_text(aes(label = round(log_pvalue, 1)), 
-            vjust = -0.5, size = 3.5, color = "black") 
-
-print(g_add)
 
 ggsave("CCA-out.png", plot = g, device = "png", 
-       width = 10, height = 8)
+       width = 15, height = 8)
 
-save.image(file = "Statistic_Analysis.RData")
+#save.image(file = "Statistic_Analysis.RData")
 
 
 #### within microorganism #### 
@@ -574,15 +574,160 @@ tbl %>%
   geom_point() +
   theme_minimal() 
 
+tbl %>% 
+  ggplot(aes(against_collinear/similar, collinear/similar, color = name)) +
+  geom_point() +
+  theme_minimal() 
+
+library(scales)
+
+viridis_palette <- viridis_pal(option = "turbo")(13)
+
+colnames(tbl) <- c("name", "DSK targets", "co-directional", "oppositely directional", "non-collinear")
+
+
+
 library(ggradar)
-ggradar(tbl,group.point.size = 1, group.line.width = .5, line.alpha = .3) +
-  theme(legend.text = element_text(face = "italic"))
+ggradar(tbl,
+        group.point.size = 0.5,
+        group.line.width = 0.5,
+        line.alpha = 0.3,
+        axis.label.size = 3,
+        legend.text = element_text(size = 8, face = "italic")) +
+  scale_color_manual(values=viridis_palette) +
+  theme_bw()
 
-ggsave("windrose.png", width = 1500, height = 1000, units = "px")
+ggsave("windrose.png", width = 3000, height = 2000, units = "px")
 
-t.test(tbl[1,-1], tbl[2,-1])$p.value
 
-p_values_ttest <- lapply(test_results, function(x) x$p.value)
-adjust <- p.adjust(p_values_ttest, method = "fdr" )
+library(rstatix)
+
+
+tbl_long = tbl %>% pivot_longer(., 
+                     cols=-name,
+                     names_to = "direction_type", 
+                     values_to = "count" )
+
+res_ttest <- tbl_long %>%
+  t_test(
+  formula = count ~ name,
+  p.adjust.method = "fdr"
+  )
+
+### vis ### 
+
+
+library(ggpubr)
+vis <- ggplot(tbl_long, aes(x=direction_type, y=count, color=name)) +
+  geom_point(position=position_jitterdodge(jitter.width=0.2), size=3) +
+  stat_summary(fun.data=mean_cl_normal, geom="errorbar", 
+               width=0.3, position=position_dodge(0.8)) +
+  stat_summary(fun=mean, geom="point", size=4, 
+               position=position_dodge(0.8)) +
+  scale_color_brewer(palette="Set1") +
+  theme_minimal()
+
+
+### common_transcript ### upset ### 
+
+library(UpSetR)
+library(tidyverse)
+library(RColorBrewer)
+
+colset <- brewer.pal(6, "Set3")
+
+unique_transcripts <- unique(unlist(org_data_list))
+
+# matrix
+long_data <- imap_dfr(org_data_list, function(categories, org) {
+  imap_dfr(categories, function(transcripts, category) {
+    data.frame(
+      org = org,
+      category = category, 
+      transcript = transcripts
+    )
+  })
+})
+
+
+
+binary_matrix <- long_data %>%
+  mutate(present = 1) %>%
+  unite("org_category", org, category, sep = "_") %>%
+  pivot_wider(
+    names_from = org_category,
+    values_from = present,
+    values_fill = 0,
+    id_cols = transcript
+  ) 
+
+
+binary_matrix <- binary_matrix %>% 
+  column_to_rownames("transcript") %>%  
+  mutate(across(everything(), as.logical)) %>% t()
+
+
+
+upset_data <- as.data.frame(binary_matrix) %>%
+  rownames_to_column("transcript") %>%
+  mutate(across(-transcript, as.logical)) %>%
+  column_to_rownames("transcript")
+
+
+gg <- ComplexUpset::upset(
+  upset_data,
+  intersect = colnames(upset_data)[1:20],
+  name = "Transcripts",
+  width_ratio = 0.2, 
+  
+  base_annotations = list(
+    'Intersection size' = (
+      ggplot2::ggplot() +
+        ggplot2::geom_bar(
+          stat = 'count',
+          fill = "#A47DAB",
+          width = 0.7 
+        ) +
+        ggplot2::geom_text(
+          stat = 'count',
+          aes(label = after_stat(count)),
+          vjust = -0.5,
+          hjust = -0.2,
+          size = 3, 
+          fontface = "bold"
+        ) +
+        ylab("Number of organism") +
+        expand_limits(y = max(table(apply(upset_data, 1, paste, collapse = ""))) * 1.2) +
+        theme(
+          axis.title.y = element_text(size = 12), 
+          axis.text.y = element_text(size = 12)
+        )
+    )
+  ), 
+  
+  set_sizes = ComplexUpset::upset_set_size() +
+    geom_text(
+      aes(label = after_stat(count)),
+      hjust = 1.1,
+      stat = 'count',
+      size = 5
+      ) +
+    ylab("Set size") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)),
+      
+  themes = ComplexUpset::upset_modify_themes(
+    list(
+      "intersections_matrix" = theme(text = element_text(size = 12)),
+      "Intersection size" = theme(axis.text.y = element_text(size = 12))
+    )
+  ),
+  min_size = 1,
+  sort_sets = "descending"
+)   
+  
+print(gg)     
+
+ggsave("Upset.png", plot = gg, device = "png", 
+       width = 15, height = 8)
 
 
